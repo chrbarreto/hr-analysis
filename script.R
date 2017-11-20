e30434e14c7969b6c621d5b1a8165a0a91df4b53
Tulio Carreira <trcarreira@gmail.com>
2017-11-20 15:31:30 -0200

    Call iterate_folds_nn to ALL_VARS, RELIEF and PCA
    Also, general changes were made:
    - Set default 0 value to TP and FP when corresponding values in
      prediction table are empty
    - Force selection of RELIEF columns based on previous RELIEF choices
    - Add missing package imports

--- script.R
+++ script.R
@@ -1,20 +1,22 @@
 ##### 1. Leitura e exibição do dataset #####
+
 library(readr)
 HR <- read_csv("HR.csv")
 # reordena as instâncias de forma aleatória, para posteriormente manter a proporção de cada classe dentro dos folds
-HR_random <- HR[sample(nrow(HR)),]
+HR <- HR[sample(nrow(HR)),]
 View(HR)
+
 
 ##### 2. Aplicação do PCA #####
 
 # descarta as colunas nao numericas
 HR_without_chars <- HR[,c(1:8)]
 
-# executa o pca, retirando a coluna de classe, e normalizando os dados com scale()
+# executa o PCA retirando a coluna de classe e normalizando os dados com scale()
 HR_princomp <- princomp(scale(HR_without_chars[,-7]))
 print(HR_princomp$loadings)
 
-# transforma as variaveis em componentes pca, aplicando o loading
+# transforma as variaveis em componentes PCA, aplicando o loading
 HR_transformado_pca <- predict(HR_princomp, newdata = HR_without_chars[,-7])
 
 # descarta o ultimo componente, mantendo uma variacao de 85.7%
@@ -25,20 +27,33 @@ left <- HR[,c(7)]
 
 # faz merge do dataset transformado para incluir a classe
 dataset_after_pca <- cbind(HR_transformado_pca, left)
+
+# exibe o dataset com as componentes do PCA e a classe
 View(dataset_after_pca)
 
 ##### 3. Aplicando o RELIEF #####
 
+# talvez seja necessário executar as duas linhas abaixo caso o R não encontre o pacote FSelector
+# o padrâmetro de dyn.load deve ser alterado conforme a localização de seu JDK
+dyn.load('/Library/Java/JavaVirtualMachines/jdk1.8.0_112.jdk/Contents/Home/jre/lib/server/libjvm.dylib')
+require(rJava)
+
 # Importa a biblioteca FSelector
 library(FSelector)
 
-# executa o algoritmo relief para calcular os pesos das características, 
-# informando que left é a classe.
+# executa o algoritmo RELIEF para calcular os pesos das características, informando que left é a classe.
 weights <- relief(left ~ ., HR)
 
 # seleciona os 5 atributos de maior importância
 top_features <- cutoff.k(weights, 5)
-dataset_after_relief <- HR[, c(top_features)]
+
+# depois de algumas execuções do RELIEF, constatamos que as características mais influentes são as listadas abaixo
+# seleciona os 5 atributos de maior importância (e incluímos a classe "left")
+dataset_after_relief <- HR[, c("satisfaction_level", "last_evaluation", "number_project", "average_montly_hours", 
+                              "time_spend_company", "left")]
+
+# exibe o dataset com características selecionadas pelo RELIEF
+View(dataset_after_relief)
 
 ##### 4. Classificador SVM #####
 
@@ -50,7 +65,7 @@ iterate_folds_svm <- function(dataset, k, cost_input, kernel_type){
   recallSum <- 0
   errorSum <- 0
   # quebra o dataset em k folds
-  folds <- cut(seq(1,nrow(dataset)), breaks=k,labels=FALSE)
+  folds <- cut(seq(1, nrow(dataset)), breaks=k,labels=FALSE)
   for(i in 1:k){
     testIndexes <- which(folds==i,arr.ind=TRUE)
     # separa 1 fold pra teste e o restante para treinamento 
@@ -59,17 +74,18 @@ iterate_folds_svm <- function(dataset, k, cost_input, kernel_type){
     
     # Aplica-se SVM
     fit <- svm(left~., data = trainData, kernel=kernel_type, scale = TRUE, cost=cost_input, type = "C")
-    print(fit)
     # Aplica o modelo obtido sobre a base de teste
     prediction <- predict(fit, testData, type="class")
     # --------------------------------------------
     
     # matriz de confusão
-    tab <- table(prediction, testData$left)
-    TP <- tab[c(2), c(2)] # true positive
-    FP <- tab[c(2), c(1)] # false positive
-    FN <- tab[c(1), c(2)] # false negative
-    TN <- tab[c(1), c(1)] # true negative
+    # a classe left é a sétima coluna no dataset 
+    tab <- table(prediction, testData$left) 
+    print(tab)
+    TP <- ifelse(dim(tab)[1] == 1, 0, tab[c(2), c(2)])  # true positive
+    FP <- ifelse(dim(tab)[1] == 1, 0, tab[c(2), c(1)]) # false positive
+    FN <- tab[c(1), c(2)]  # false negative
+    TN <- tab[c(1), c(1)]  # true negative
     P <- TP + FN
     # cálculo de erro, precisão e recall para cada fold
     error <- (FN + FP) / (TP + FP + FN + TN)
@@ -91,11 +107,11 @@ iterate_folds_svm <- function(dataset, k, cost_input, kernel_type){
 }
 
 # Parâmetros a serem variados para o SVM
-k <- 5 # quantidade de folds, mantivemos 5 por causa do tempo de processamento
-c <- 1 # custo. Utilizamos os valores 0.1, 0.5 e 1
-kernel <- "radial" # trocar para cada tipo. Possibilidades: linear, sigmoid, polynomial e radial
+k <- 5 # Quantidade de folds: mantivemos 5 por causa do tempo de processamento
+c <- 1 # Custo do erro: variamos entre os valores 0.1, 0.5 e 1
+kernel <- "radial" # Kernel: trocamos para cada tipo. Possibilidades: linear, sigmoid, polynomial e radial
 
-# Executa o SVM com TODAS as características,  calcula e exibe erro, precisão, e recall médios 
+# Executa o SVM com TODAS as características, calcula e exibe erro, precisão, e recall médios 
 values <- iterate_folds_svm(HR, k, c, kernel)
 print(paste("Erro médio: ", values[3]))
 print(paste("Sensibilidade média: ", values[2]))
@@ -116,7 +132,7 @@ print(paste("Precisão média: ", values[1]))
 ##### 5. Classificador Redes Neurais #####
 
 # função iterate_folds modificada para NN
-iterate_folds_nn <- function(dataset, k, cost_input, kernel_type){
+iterate_folds <- function(dataset, k, num_neurons_input, learn_input){
   # inicializa algumas variáveis
   precisionSum <- 0
   recallSum <- 0
@@ -124,14 +140,14 @@ iterate_folds_nn <- function(dataset, k, cost_input, kernel_type){
   # quebra o dataset em k folds
   folds <- cut(seq(1,nrow(dataset)), breaks=k,labels=FALSE)
   for(i in 1:k){
+    # separa-se 1 fold pra teste e o restante para treinamento 
     testIndexes <- which(folds==i,arr.ind=TRUE)
-    # separa 1 fold pra teste e o restante para treinamento 
     testData <- dataset[testIndexes, ]
     trainData <- dataset[-testIndexes, ]
     
-    # Aplica-se neuralnet
-    # TODO: ver se tem forma generica de fazer isso, pra servir pro PCA também 
-    fit <- neuralnet( left ~ satisfaction_level + last_evaluation + number_project + 
+    # Aplica-se neuralnet para todas as características
+    # A fórmula deve ser alterada de acordo com a quantidade de características inputadas
+    fit <- neuralnet(left ~ satisfaction_level + last_evaluation + number_project + 
                         average_montly_hours + time_spend_company + Work_accident +
                         promotion_last_5years + departmentaccounting + departmenthr + 
                         departmentIT + departmentmanagement + departmentmarketing + 
@@ -139,6 +155,7 @@ iterate_folds_nn <- function(dataset, k, cost_input, kernel_type){
                         departmentsupport + departmenttechnical + salaryhigh + salarylow + salarymedium,
                       data = trainData, hidden=num_neurons_input, learningrate = learn_input, err.fct ="ce",
                       linear.output = FALSE, stepmax=1e6)
+    
     # Aplica o modelo obtido sobre a base de teste
     output <- compute(fit, testData[, -21])
     p <- output$net.result
@@ -146,11 +163,13 @@ iterate_folds_nn <- function(dataset, k, cost_input, kernel_type){
     
     # matriz de confusão
     tab <- table(prediction, testData$left)
-    TP <- tab[c(2), c(2)] # true positive
-    FP <- tab[c(2), c(1)] # false positive
-    FN <- tab[c(1), c(2)] # false negative
-    TN <- tab[c(1), c(1)] # true negative
+    print(tab)
+    TP <- ifelse(dim(tab)[1] == 1, 0, tab[c(2), c(2)])  # true positive
+    FP <- ifelse(dim(tab)[1] == 1, 0, tab[c(2), c(1)]) # false positive
+    FN <- tab[c(1), c(2)]  # false negative
+    TN <- tab[c(1), c(1)]  # true negative
     P <- TP + FN
+    
     # cálculo de erro, precisão e recall para cada fold
     error <- (FN + FP) / (TP + FP + FN + TN)
     precision <- TP/(TP + FP)
@@ -170,14 +189,18 @@ iterate_folds_nn <- function(dataset, k, cost_input, kernel_type){
   return(list(precisionSum/k, recallSum/k, errorSum/k))
 }
 
-
+# importa pacotes para variáveis dummy e redes neurais
+library(neuralnet)
+library(dummies)
 
 # cria variáveis dummy para poder executar o NN
-HR_dummy <- dummy.data.frame(HR1)
-View(HR_dummy)
+HR_dummy <- dummy.data.frame(HR)
 left <- HR_dummy$left
-HR_dummy <- scale(HR_dummyHR[, c(1:6, 8:21)])
+HR_dummy <- scale(HR_dummy[, c(1:6, 8:21)])
 HR_dummy <- cbind(HR_dummy, left)
+
+# reordena as instâncias de forma aleatória
+HR_dummy <- HR_dummy[sample(nrow(HR_dummy)),]
 
 # Parâmetros a serem variados para o NN
 k <- 5 # quantidade de folds, mantivemos 5 por causa do tempo de processamento
@@ -185,11 +208,25 @@ num_neurons_input <- 1 # número de neurônios, variamos de 1 a 4 neurônios
 learn_input <- 0.1 # learning rate. Utilizamos os seguintes valores: 0.1, 0.5 e 1
 
 # Executa o NN com TODAS as características,  calcula e exibe erro, precisão, e recall médios 
-values <- iterate_folds_nn(HR, k, c, kernel)
-# TODO: completar o restante de NN, tem como fazer generico ou vamos ter que repetir a funcao iterate folds?
+values <- iterate_folds(HR_dummy, k, num_neurons_input, learn_input)
+print(paste("Erro médio: ", values[3]))
+print(paste("Sensibilidade média: ", values[2]))
+print(paste("Precisão média: ", values[1]))
+
+# Executa o NN com as características do RELIEF, calcula e exibe erro, precisão, e recall médios 
+values <- iterate_folds(dataset_after_relief, k, num_neurons_input, learn_input)
+print(paste("Erro médio: ", values[3]))
+print(paste("Sensibilidade média: ", values[2]))
+print(paste("Precisão média: ", values[1]))
+
+# Executa o NN com os componentes do PCA, calcula e exibe erro, precisão, e recall médios 
+values <- iterate_folds(dataset_after_pca, k, num_neurons_input, learn_input)
+print(paste("Erro médio: ", values[3]))
+print(paste("Sensibilidade média: ", values[2]))
+print(paste("Precisão média: ", values[1]))
 
 
-##### 6. Classificador naive bayes ######
+##### 6. Classificador Naive Bayes ######
 library(e1071)
 # função iterate_folds modificada para naive bayes
 iterate_folds_bayes <- function(dataset, k){
@@ -205,20 +242,17 @@ iterate_folds_bayes <- function(dataset, k){
     testData <- dataset[testIndexes, ]
     trainData <- dataset[-testIndexes, ]
     
-    # --------------------------------------------
-    # Aplica-se SVM/Redes Neurais/Naive Bayes aqui
+    # Aplica-se Naive Bayes aqui
     fit <- naiveBayes(left ~., data = dataset)
-    print(fit)
     prediction <- predict(fit, testData)
-    # --------------------------------------------
     
     # matriz de confusão
     tab <- table(prediction, testData$left)
     print(tab)
-    TP <- tab[c(2), c(2)]
-    FP <- tab[c(2), c(1)]
-    FN <- tab[c(1), c(2)]
-    TN <- tab[c(1), c(1)]
+    TP <- ifelse(dim(tab)[1] == 1, 0, tab[c(2), c(2)])  # true positive
+    FP <- ifelse(dim(tab)[1] == 1, 0, tab[c(2), c(1)]) # false positive
+    FN <- tab[c(1), c(2)]  # false negative
+    TN <- tab[c(1), c(1)]  # true negative
     P <- TP + FN
     # cálculo de erro, precisão e recall
     error <- (FN + FP) / (TP + FP + FN + TN)
@@ -248,13 +282,13 @@ print(paste("Erro médio: ", values[3]))
 print(paste("Sensibilidade média: ", values[2]))
 print(paste("Precisão média: ", values[1]))
 
-# Executa o naive bayes com características selecionadas pelo RELIEF,  calcula e exibe erro, precisão, e recall médios 
+# Executa o naive bayes com características selecionadas pelo RELIEF, calcula e exibe erro, precisão, e recall médios 
 values <- iterate_folds_bayes(dataset_after_relief, k)
 print(paste("Erro médio: ", values[3]))
 print(paste("Sensibilidade média: ", values[2]))
 print(paste("Precisão média: ", values[1]))
 
-# Executa o naive bayes com componentes selecionados usando PCA,  calcula e exibe erro, precisão, e recall médios 
+# Executa o naive bayes com componentes selecionados usando PCA, calcula e exibe erro, precisão, e recall médios 
 values <- iterate_folds_bayes(dataset_after_pca, k)
 print(paste("Erro médio: ", values[3]))
 print(paste("Sensibilidade média: ", values[2]))
